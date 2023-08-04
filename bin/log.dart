f21f1ce4d8a4361a089d6a5543275588c275b67a
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-04 05:46:53 +0200

    Add log file, and log class

---
+++ bin/log.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'dart:io';
+
+class Log {
+  Log._();
+
+  static Log? _ins;
+
+  factory Log() {
+    if (_ins == null) {
+      _ins = Log._();
+      return _ins!;
+    }
+    return _ins!;
+  }
+
+  List<String> logLines = [];
+
+  void log(Object obj) {
+    logLines.add('${DateTime.now().millisecondsSinceEpoch}: $obj');
+  }
+
+  Future<List<String>> readFile() async {
+    var logFile = File('${DateTime.now().millisecondsSinceEpoch}.log');
+    var contents = await logFile.readAsString();
+    return json.decode(contents);
+  }
+
+  Future<void> saveToFile() async {
+    var logFile = File('${DateTime.now().millisecondsSinceEpoch}.log');
+    await logFile.writeAsString(
+      json.encode(log),
+    );
+    logLines.clear();
+  }
+}
