a3a78cfbfd3f990f84f04d13ff184a8e1bc8188e
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-07 02:37:15 +0200

    Edit pubspec, server_todo

--- bin/server_todo.dart
+++ bin/server_todo.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart' as http;
+
 class ServerTODO {
   final xport = 8156;
 
@@ -5,7 +7,27 @@ class ServerTODO {
   Future<void> tick() async {
     running = true;
     while (running) {
-      
+      await Future.delayed(Duration(seconds: 30));
+      for (var task in tasks) {
+        if (task.time.millisecondsSinceEpoch <
+            DateTime.now().millisecondsSinceEpoch) {
+          var res = await http.post(
+            Uri.parse('http://localhost:$xport/post'),
+            body: task.content,
+          );
+          task.statusCode = res.statusCode;
+        }
+      }
     }
   }
+
+  var tasks = <Task>[];
+  // var inNext10Mins = <Task>[];
+}
+
+class Task {
+  Task(this.time, this.content);
+  final DateTime time;
+  final String content;
+  late int statusCode;
 }
