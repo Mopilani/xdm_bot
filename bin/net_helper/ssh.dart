faea36bd67b581abd9506f998c9a5f1a47fee453
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-12 13:59:04 +0200

    Edit base commands

--- bin/net_helper/ssh.dart
+++ bin/net_helper/ssh.dart
@@ -26,15 +26,57 @@ class SSHClient {
       'Set your ssh client',
       'ssh:a6a2a3b4b2b5.c02c427b.alx-cod.online',
       (authorId, command, body) async {
+        userChatStates[authorId]!['state'] = UserChatState.normalMode;
+        return 'Exited';
+      },
+      UserChatState.sshCommands,
+      adminCommand: true,
+    ),
+    BotCommand(
+      'stop',
+      'Set your ssh client',
+      'ssh:a6a2a3b4b2b5.c02c427b.alx-cod.online',
+      (authorId, command, body) async {
+        try {
+          clients[authorId]!.stop();
+        } catch (e) {
+          //
+        }
+        return 'Exited (0)';
+      },
+      UserChatState.sshCommands,
+      adminCommand: true,
+    ),
+    BotCommand(
+      'start',
+      'Set your ssh client',
+      'ssh:a6a2a3b4b2b5.c02c427b.alx-cod.online',
+      (authorId, command, body) async {
+        try {
+          clients[authorId]!.start();
+          return 'Started';
+        } catch (e) {
+          return 'Err: $e';
+        }
+      },
+      UserChatState.sshCommands,
+      adminCommand: true,
+    ),
+    BotCommand(
+      'ssh',
+      'Enter ssh mode',
+      'ssh:o',
+      (authorId, command, body) async {
+        userChatStates[authorId]!['state'] = UserChatState.sshCommands;
         return 'OK';
       },
       UserChatState.all,
       adminCommand: true,
     ),
     BotCommand(
-      'ssh',
-      'Set your ssh client',
-      'ssh:a6a2a3b4b2b5.c023c54eb.alx-cod.online',
+      'set',
+      'Set your ssh client host',
+      'set:a6a2a3b4b2b5.c023c54eb.alx-cod.online',
       (authorId, command, body) async {
         var sshHots = body[command];
         var client = SSHClientBridge(sshHots);
@@ -47,7 +89,7 @@ class SSHClient {
           return 'Err: $e';
         }
       },
-      UserChatState.all,
+      UserChatState.sshCommands,
       adminCommand: true,
     ),
   ];
