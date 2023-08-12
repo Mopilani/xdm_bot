4fe39370d659c592e26f57377638e501e629ba7f
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-12 16:28:55 +0200

    Edit auto send time

--- bin/author.dart
+++ bin/author.dart
@@ -460,8 +460,8 @@ class Author {
                 nd.year,
                 nd.month,
                 nd.day,
-                11,
-                15,
+                6,
+                0,
                 0,
               ).millisecondsSinceEpoch <
               nd.millisecondsSinceEpoch) &&
@@ -469,8 +469,8 @@ class Author {
                 nd.year,
                 nd.month,
                 nd.day,
-                11,
-                15,
+                6,
+                0,
                 30,
               ).millisecondsSinceEpoch >
               nd.millisecondsSinceEpoch) {
