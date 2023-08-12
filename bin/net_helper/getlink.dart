64e63ca1c68dbba3441c0fc4c5b93772d7d73270
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-12 17:36:28 +0200

    Write getlink base

---
+++ bin/net_helper/getlink.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'dart:io';
+
+// import 'package:http/http.dart' as http;
+// https://dl3.downloadly.ir/Files/Elearning/Coursera_Foundations_of_Cybersecurity_2023_5_Downloadly.ir.rar
+void main(List<String> args) async {
+  var link = args[0];
+  var fileName = link.split('/').last;
+
+  var file = File(fileName);
+
+  var client = HttpClient();
+  var req = await client.getUrl(Uri.parse(link));
+
+  var res = await req.close();
+  var sink = file.openWrite();
+
+  var count = 0;
+
+  Timer(Duration(seconds: 2), () { 
+    print(count);
+  });
+
+  res.listen(
+    (event) {
+      count += event.length;
+      sink.write(event);
+    },
+    onDone: () {
+      print('Done successfuly');
+    },
+    onError: (e, s) {
+      print(e);
+      print(s);
+    }
+  );
+}
