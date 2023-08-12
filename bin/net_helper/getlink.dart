4f5e8c018e46805bf8235808464d7915aadf4db2
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-12 20:52:00 +0200

    Delete commented lines

--- bin/net_helper/getlink.dart
+++ bin/net_helper/getlink.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-// import 'package:http/http.dart' as http;
 // https://dl3.downloadly.ir/Files/Elearning/Coursera_Foundations_of_Cybersecurity_2023_5_Downloadly.ir.rar
 void main(List<String> args) async {
   var link = args[0];
@@ -13,10 +12,7 @@ void main(List<String> args) async {
   var req = await client.getUrl(Uri.parse(link));
 
   var res = await req.close();
-  var sink = await file.open(
-    mode: FileMode.writeOnly,
-    // encoding: latin1,
-  );
+  var sink = await file.open(mode: FileMode.writeOnly);
 
   var count = 0;
   var stoptimer = false;
@@ -35,7 +31,6 @@ void main(List<String> args) async {
   res.listen((event) async {
     count += event.length;
     sink.writeFromSync(event);
-    // sink.write(event);
   }, onDone: () {
     stoptimer = true;
     print('Done successfuly');
