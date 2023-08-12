01f998d06003fcbf18525bf1442eb29fe6b02631
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-12 23:10:07 +0200

    Rebuild dloader base code

--- bin/net_helper/dloader.dart
+++ bin/net_helper/dloader.dart
@@ -1,109 +1,103 @@
-// import '../bot_command.dart';
-// import '../functions.dart';
-// import 'dloader_task.dart';
-// import 'ssh_connector.dart';
+import 'dart:convert';
 
-// class Dloader {
-//   String? id;
-//   Dloader(
-//     this.id,
-//   );
+import '../bot_command.dart';
+import '../functions.dart';
+import 'dloader_task.dart';
 
-//   static Dloader fromMap(Map<String, dynamic> data) {
-//     return Dloader(
-//       data['id'],
-//     );
-//   }
+import 'package:http/http.dart' as http;
 
-//   static Map<String, DloaderTask> clients = {};
+class Dloader {
+  String? id;
+  Dloader(
+    this.id,
+  );
 
-//   Map<String, dynamic> asMap() => {
-//         'id': id,
-//       };
+  static Dloader fromMap(Map<String, dynamic> data) {
+    return Dloader(
+      data['id'],
+    );
+  }
 
-//   static final List<BotCommand> commands = [
-//     BotCommand(
-//       'exit',
-//       'Set your ssh client',
-//       'ssh:a6a2a3b4b2b5.c02c427b.alx-cod.online',
-//       (authorId, command, body) async {
-//         userChatStates[authorId]!['state'] = UserChatState.normalMode;
-//         return 'Exited mode';
-//       },
-//       UserChatState.sshCommands,
-//       adminCommand: true,
-//     ),
-//     BotCommand(
-//       'stop',
-//       'Set your ssh client',
-//       'ssh:a6a2a3b4b2b5.c02c427b.alx-cod.online',
-//       (authorId, command, body) async {
-//         try {
-//           clients[authorId]!.stop();
-//         } catch (e) {
-//           //
-//         }
-//         return 'Exited (0)';
-//       },
-//       UserChatState.sshCommands,
-//       adminCommand: true,
-//     ),
-//     BotCommand(
-//       'start',
-//       'Set your ssh client',
-//       'ssh:a6a2a3b4b2b5.c02c427b.alx-cod.online',
-//       (authorId, command, body) async {
-//         try {
-//           clients[authorId]!.start();
-//           return 'Started';
-//         } catch (e) {
-//           return 'Err: $e';
-//         }
-//       },
-//       UserChatState.sshCommands,
-//       adminCommand: true,
-//     ),
-//     BotCommand(
-//       'ssh',
-//       'Enter ssh mode',
-//       'ssh:o',
-//       (authorId, command, body) async {
-//         userChatStates[authorId]!['state'] = UserChatState.sshCommands;
-//         return 'OK';
-//       },
-//       UserChatState.all,
-//       adminCommand: true,
-//     ),
-//     BotCommand(
-//       's',
-//       'Enter ssh command',
-//       's:Your command',
-//       (authorId, command, body) async {
-//         var cmd = body[command];
-//         clients[authorId]!.sendCommand(cmd);
-//         return 'k';
-//       },
-//       UserChatState.sshCommands,
-//       adminCommand: true,
-//     ),
-//     BotCommand(
-//       'set',
-//       'Set your ssh client host',
-//       'set:a6a2a3b4b2b5.c023c54eb.alx-cod.online',
-//       (authorId, command, body) async {
-//         var sshHots = body[command];
-//         var client = DloaderTask(sshHots);
-//         try {
-//           await client.start();
-//           client.bind(client.whatsClientCallBack(authorId));
-//           clients.addAll({authorId: client});
-//           return 'OK';
-//         } catch (e) {
-//           return 'Err: $e';
-//         }
-//       },
-//       UserChatState.sshCommands,
-//       adminCommand: true,
-//     ),
-//   ];
-// }
+  static var serverUrl = Uri.parse('localhost:8186');
+
+  static http.Client client = http.Client();
+
+  // static Map<String, DloaderTask> clients = {};
+
+  Map<String, dynamic> asMap() => {
+        'id': id,
+      };
+
+  static final List<BotCommand> commands = [
+    BotCommand(
+      'exit',
+      'Set your ssh client',
+      '',
+      (authorId, command, body) async {
+        userChatStates[authorId]!['state'] = UserChatState.normalMode;
+        return 'Exited mode';
+      },
+      UserChatState.sshCommands,
+      adminCommand: true,
+    ),
+    BotCommand(
+      'stop',
+      'Set your ssh client',
+      '',
+      (authorId, command, body) async {
+        return 'Exited (0)';
+      },
+      UserChatState.sshCommands,
+      adminCommand: true,
+    ),
+    BotCommand(
+      'tasks',
+      'List server tasks queue',
+      'tasks',
+      (authorId, command, body) async {
+        var res = await client.get(serverUrl);
+        return res.body;
+      },
+      UserChatState.sshCommands,
+      adminCommand: true,
+    ),
+    BotCommand(
+      'add',
+      'Add task to the tasks queue',
+      'add:https://link_here.com/example',
+      (authorId, command, body) async {
+        var link = body[command];
+        var res = await client.post(
+          serverUrl,
+          body: json.encode({'link': link}),
+        );
+        return res.body;
+      },
+      UserChatState.all,
+      adminCommand: true,
+    ),
+    BotCommand(
+      'status',
+      'Enter ssh command',
+      'status:https://link_here.com/example',
+      (authorId, command, body) async {
+        var link = body[command];
+        var res = await client.get(serverUrl, headers: {'link': link});
+        return res.body;
+      },
+      UserChatState.all,
+      adminCommand: true,
+    ),
+    BotCommand(
+      'set',
+      'Set your server client host',
+      '',
+      (authorId, command, body) async {
+        var sshHots = body[command];
+        return 'OK';
+      },
+      UserChatState.all,
+      adminCommand: true,
+    ),
+  ];
+}
