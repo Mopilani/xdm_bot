efc16363c1153179445566c558543a71563c1c9c
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-13 03:21:15 +0200

    Remove test links

--- bin/net_helper/dloader_client.dart
+++ bin/net_helper/dloader_client.dart
@@ -1,20 +1,10 @@
 import 'package:http/http.dart' as http;
+import '../../links.dart';
 
 void main(List<String> args) async {
   var r = await http.post(
-    Uri.parse('http://167.172.167.245:8188/add'),
-    headers: {
-      'link':
-          ''
-          // ''
-          // ''
-          'https://dl3.downloadly.ir/Files/Elearning/Coursera_Sound_the_Alarm_Detection_and_Response_2023_5_Downloadly.ir.rar'
-          // 'https://dl3.downloadly.ir/Files/Elearning/Coursera_Assets_Threats_and_Vulnerabilities_2023_5_Downloadly.ir.rar'
-          // 'https://dl3.downloadly.ir/Files/Elearning/Coursera_Tools_of_the_Trade_Linux_and_SQL_2023_5_Downloadly.ir.rar'
-          // 'https://dl3.downloadly.ir/Files/Elearning/Coursera_Connect_and_Protect_Networks_and_Network_Security_2023_5_Downloadly.ir.rar'
-          // 'https://dl3.downloadly.ir/Files/Elearning/Coursera_Play_It_Safe_Manage_Security_Risks_2023_5_Downloadly.ir.rar'
-          // 'https://dl3.downloadly.ir/Files/Elearning/Coursera_Foundations_of_Cybersecurity_2023_5_Downloadly.ir.rar'
-    },
+    Uri.parse('http://167.172.167.245:8186/add'),
+    headers: {'link': link},
   );
 
   print(r.statusCode);
