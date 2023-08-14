c2ee2444c7afb881ce3444b4bf024f6e20dc85a4
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-14 23:30:30 +0200

    Unimportant changes

--- bin/net_helper/dloader_client.dart
+++ bin/net_helper/dloader_client.dart
@@ -2,30 +2,34 @@ import 'package:http/http.dart' as http;
 import '../../links.dart';
 
 void main(List<String> args) async {
-  var r = await http.get(
-    Uri.parse('http://localhost:8000/video'),
-    headers: {
-      'link': link,
-      'range': '100-',
-    },
-  );
-
-  print(r.statusCode);
-  print('----------------------------');
-  print(r.headers);
-  print('----------------------------');
-  print(r.bodyBytes.length);
-  print('----------------------------');
-
-  // var r = await http.post(
-  //   Uri.parse('http://167.172.167.245:8186/add'),
-  //   headers: {'link': link},
+  // var r = await http.get(
+  //   Uri.parse('http://localhost:8000/video'),
+  //   headers: {
+  //     'link': link,
+  //     'range': '100-',
+  //   },
   // );
 
   // print(r.statusCode);
   // print('----------------------------');
   // print(r.headers);
   // print('----------------------------');
-  // print(r.body);
+  // print(r.bodyBytes.length);
   // print('----------------------------');
+
+  var r = await http.post(
+    // Uri.parse('http://167.172.167.245:8186/add'),
+    Uri.parse('http://localhost:8186/resume/0'),
+    // Uri.parse('http://localhost:8186/resume'),
+    // Uri.parse('http://localhost:8186/add'),
+    // Uri.parse('http://localhost:8186/redown'),
+    headers: {'link': link},
+  );
+
+  print(r.statusCode);
+  print('----------------------------');
+  print(r.headers);
+  print('----------------------------');
+  print(r.body);
+  print('----------------------------');
 }
