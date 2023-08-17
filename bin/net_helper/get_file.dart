8bd6f70100356419699cdd6aeaad38d7af4400f8
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-17 21:42:03 +0200

    Add getfile method

---
+++ bin/net_helper/get_file.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(List<String> args) async {
+  if (args.isEmpty) {
+    print('Pass the file url');
+    return;
+  }
+
+  var fileurl = args[0];
+  var filename = fileurl.split('/').last;
+  var file = File(filename);
+  var sink = file.openWrite();
+
+  var client = HttpClient();
+  var req = await client.getUrl(Uri.parse(fileurl));
+  var res = await req.close();
+
+  res.listen(
+    (chunk) {
+      sink.add(chunk);
+    },
+    onDone: () {
+      sink.close();
+    },
+    onError: (e, s) {
+      print(e);
+      print(s);
+    }
+  );
+}
