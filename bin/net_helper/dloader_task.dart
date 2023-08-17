132dfcea104673b0db746d357a7b78f1a7c37920
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-17 09:32:18 +0200

    Hot fix 71

--- bin/net_helper/dloader_task.dart
+++ bin/net_helper/dloader_task.dart
@@ -197,7 +197,7 @@ class DloaderTask {
     stoptimer = true;
     running = false;
     waiting = false;
-    started = false;
+    // started = false;
     raf.closeSync();
     return ('Stopped successfuly');
   }
@@ -216,7 +216,7 @@ class DloaderTask {
     stoptimer = true;
     running = false;
     waiting = false;
-    started = false;
+    // started = false;
     if (downloaded == size) {
       finished = true;
     }
@@ -230,7 +230,7 @@ class DloaderTask {
     stoptimer = true;
     running = false;
     waiting = false;
-    started = false;
+    // started = false;
     raf.closeSync();
     return e.toString();
   }
